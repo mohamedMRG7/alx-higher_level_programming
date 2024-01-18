27e6f77f6daa86f7b36105eab9555f8a5849c1c6
mohamedalx <mohamedalx.gmail.com>
2024-01-18 07:16:47 -0800

    task

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(f"{str[39:66]}{str[105:111]}{str[0:6]}")
+str = f"{str[39:66]}{str[105:112]}{str[0:6]}"
+print(str)
