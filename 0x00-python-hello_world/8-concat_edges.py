17049921145f0490df821af88e899d0dd0edbe03
mohamedalx <mohamedalx.gmail.com>
2024-01-18 07:06:30 -0800

    task

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 print(f"{str[39:66]}{str[105:111]}{str[0:6]}")
